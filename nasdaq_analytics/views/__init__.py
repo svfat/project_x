98ac54480b4095d7c210f8ec805fd2b3b51e5677
vladimir <vovanzagorodsky@gmail.com>
2018-07-20 22:40:08 +0500

    some docs, fixes

--- nasdaq_analytics/views/__init__.py
+++ nasdaq_analytics/views/__init__.py
@@ -1,10 +1,17 @@
+"""Модуль, содержащий представления для веб-интерфейса и API."""
 from flask import Flask
 
 from .helpers import views_helper
-from .routes import *  # noqa
+from .routes import (
+    index, historical_prices, insider_trades, insider_trades_by_insider_name, analytics, delta,
+)
 
 
-def init_app(app: Flask):
+def init_app(app: Flask) -> None:
+    """Проинициализировать приложение.
+
+    :param app: объект приложения Flask
+    """
     views_helper.init_app(app)
 
 
--- nasdaq_analytics/views/helpers.py
+++ nasdaq_analytics/views/helpers.py
@@ -1,8 +1,9 @@
+"""Модуль, содержащий вспомогательные классы и функции для представлений."""
 import re
 from functools import wraps
 from typing import Callable, Union, Dict, Any, List
 
-from flask import Flask, Blueprint, render_template, jsonify
+from flask import Flask, Blueprint, render_template, jsonify, Response
 from jsonschema import validate
 from werkzeug.routing import Rule
 
@@ -15,15 +16,29 @@ def _convert_werkzeug_rule_to_openapi_url_template(rule: Union[str, Rule]) -> st
 
 
 class ViewsHelper:
+    """Вспомогательный класс для представлений.
+
+    Смысл существования этого класса в том, чтобы не дублировать код для веб-интерфейса и для API.
+    Метод `route` создаёт одновременно представление для веб-интерфейса, которое рендерит шаблон и представление для API,
+    которое возвращает json.
+
+    Так же этот класс содержит генерацию спецификации к API в формате OpenAPI (aka Swagger).
+    """
+    #: объект приложения
     app: Flask
 
+    #: Blueprint для веб-интерфейса
     web_blueprint: Blueprint
+    #: Blueprint для API
     api_blueprint: Blueprint
 
+    #: спецификация к API в формете OpenAPI 3
     spec: Dict[str, Any]
+    #: контекст, который передаётся во все шаблоны
     global_context: Dict[str, Any]
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Инициализация."""
         self.web_blueprint = Blueprint('web_blueprint', __name__, url_prefix='/')
         self.api_blueprint = Blueprint('api_blueprint', __name__, url_prefix='/api')
         self.spec = {
@@ -43,17 +58,23 @@ class ViewsHelper:
             }
         }
 
-    def init_app(self, app: Flask):
+    def init_app(self, app: Flask) -> None:
+        """Инициализация приложения.
+
+        :param app: объект приложения, который надо проинициализировать
+        """
         self.app = app
         self.app.register_blueprint(self.web_blueprint)
         self.app.register_blueprint(self.api_blueprint)
 
         @self.app.route('/swagger-ui')
         def swagger_ui():
+            """Веб-интерфейс, содержащий автогенерированную документацию к API."""
             return render_template('swagger-ui/index.html')
 
         @self.app.route('/swagger.json')
         def swagger_json():
+            """Метод, возвращающий спецификацию API в формате OpenAPI."""
             return jsonify(self.spec)
 
     def _add_path_to_spec(
@@ -90,20 +111,49 @@ class ViewsHelper:
         parameters: List[Dict],
         description: str = '',
         **options,
-    ):
+    ) -> Callable[[Callable[..., Dict]], Callable[..., Dict]]:
+        """Создание декоратора, который регистрирует представление и для API и для веб-интерфейса.
+
+        Аналог `Flask.route` и `Blueprint.route`.
+
+        :param rule: правило для роутинга (строка или Rule)
+        :param template_name: название шаблона для веб-интерфейса
+        :param schema: jsonschema для валидации ответа для API и генерирования спецификации к API
+        :param parameters: параметры, которые принимает этот метод API, нужны для генерирования спецификации к API
+        :param description: описание метода API
+        :param options: другие параметры, которые будут переданы в `Blueprint.route`
+        :return: декоратор
+        """
         self._add_path_to_spec(rule, schema, parameters, description)
 
-        def decorator(view_func: Callable[..., Dict]):
+        def decorator(view_func: Callable[..., Dict]) -> Callable[..., Dict]:
+            """Декоратор, который регистрирует представление и для API и для веб-интерфейса.
+
+            :param view_func: представление (view)
+            :return: представление (view)
+            """
             @self.web_blueprint.route(rule, **options)
             @wraps(view_func)
-            def web_view_func(*args, **kwargs):
+            def web_view_func(*args, **kwargs) -> Response:
+                """Обработчик запроса для веб-интерфейса.
+
+                Вызывает функцию представления и использует её результаты для рендеринга нужного шаблона.
+
+                :return: ответ на запрос
+                """
                 context = self.global_context.copy()
                 context.update(view_func(*args, **kwargs))
                 return render_template(template_name, **context)
 
             @self.api_blueprint.route(rule, **options)
             @wraps(view_func)
-            def api_view_func(*args, **kwargs):
+            def api_view_func(*args, **kwargs) -> Response:
+                """Обработчик запроса для API.
+
+                Вызывает функцию представления, валидирует результат и возвращает JSON-ответ.
+
+                :return: ответ на запрос
+                """
                 result = view_func(*args, **kwargs)
                 validate(result, schema)
                 return jsonify(**view_func(*args, **kwargs))
