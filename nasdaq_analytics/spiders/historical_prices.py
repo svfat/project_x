015cb71acd80b1f726b827c71fca0535d0ca4e1e
vladimir <vovanzagorodsky@gmail.com>
2018-07-23 13:12:27 +0500

    spiders docs

--- nasdaq_analytics/spiders/historical_prices.py
+++ nasdaq_analytics/spiders/historical_prices.py
@@ -1,7 +1,7 @@
 """Модуль, содержащий "паука" для сбора исторических цен."""
 import logging
 from datetime import datetime
-from typing import NamedTuple, Dict
+from typing import NamedTuple, Dict, Iterator
 
 from scrapy.http.request import Request
 from scrapy.http.response import Response
@@ -12,6 +12,7 @@ from .common import only_numbers
 
 
 class RawRow(NamedTuple):
+    """Именнованный кортеж для хранения строки таблицы в "сыром виде"."""
     date: str
     open: str
     high: str
@@ -21,10 +22,16 @@ class RawRow(NamedTuple):
 
     @staticmethod
     def from_selector(selector: Selector) -> 'RawRow':
+        """Метод, получающий из селектора именнованный кортеж с нужными нам значениями.
+
+        :param selector: селектор, из которого можно получить строку таблицы
+        :return: результат парсинга этой строки таблицы
+        """
         return RawRow(*selector.xpath('./td/text()').extract())
 
 
 class ParsedRow(NamedTuple):
+    """Именнованный кортеж, для хранения строки таблицы в распарсенном виде."""
     date: str
     open: float
     high: float
@@ -35,6 +42,12 @@ class ParsedRow(NamedTuple):
 
     @staticmethod
     def from_raw(raw_row: RawRow, symbol: str) -> 'ParsedRow':
+        """Метод, получающий из "сырых" данных обработанные.
+
+        :param raw_row: именнованный кортеж с нужными нам данными в сыром виде
+        :param symbol: название акции ("символ"), к которым относятся эти данные
+        :return: именнованный кортеж с нужными данными в нужном формате, готовыми к отправке "наружу"
+        """
         return ParsedRow(
             date=datetime.strptime(raw_row.date.strip(), '%m/%d/%Y').strftime('%Y-%m-%d'),
             open=float(raw_row.open.strip()),
@@ -46,18 +59,31 @@ class ParsedRow(NamedTuple):
         )
 
     def as_dict(self) -> Dict:
+        """Прокси для приватного метода `_asdict`.
+
+        :return: именнованный кортеж, преобразованный в словарь
+        """
         return self._asdict()
 
 
 class HistoricalPricesSpider(BaseSpider):
-    """"""
+    """"Паук", для получения данных об исторических ценах."""
     name = 'historical_prices'
 
-    def start_requests(self):
+    def start_requests(self) -> Iterator[Request]:
+        """Итератор по первоначальным запросам.
+
+        Делает запрос к странице с историческими ценам для каждой переданной акции.
+        """
         for symbol in self.symbols:
             yield Request(f'https://www.nasdaq.com/symbol/{symbol.lower()}/historical', meta={'symbol': symbol})
 
-    def parse(self, response: Response):
+    def parse(self, response: Response) -> Iterator[Dict]:
+        """Обработчик http ответа от сайта.
+
+        :param response: ответ, получаемый из Scrapy
+        :return: итератор по словарям с результатами парсинга таблицы с нужными данными
+        """
         symbol = response.meta['symbol']
         for row in response.xpath('//div[@id="historicalContainer"]//table/tbody/tr'):
             raw_row = RawRow.from_selector(row)
--- nasdaq_analytics/spiders/insider_trades.py
+++ nasdaq_analytics/spiders/insider_trades.py
@@ -1,8 +1,8 @@
-""""""
+"""Модуль, содержащий "паука" для сбора информации о торгах инсайдеров."""
 import logging
 import re
 from datetime import datetime
-from typing import NamedTuple, Optional, Match
+from typing import NamedTuple, Optional, Match, Iterator, Union, Dict
 
 from scrapy.http.request import Request
 from scrapy.http.response import Response
@@ -13,10 +13,12 @@ from scrapy.selector import Selector
 from .base import BaseSpider
 from .common import only_numbers
 
+#: сколько страниц нужно спарсить
 MAX_PAGE: int = 10
 
 
 class RawRow(NamedTuple):
+    """Именнованный кортеж для хранения строки таблицы в "сыром виде"."""
     insider_name: str
     insider_id: str
     relation: Optional[str]
@@ -30,6 +32,11 @@ class RawRow(NamedTuple):
 
     @staticmethod
     def from_selector(selector: Selector, symbol: str) -> 'RawRow':
+        """Метод, получающий из селектора именнованный кортеж с нужными нам значениями.
+
+        :param selector: селектор, из которого можно получить строку таблицы
+        :return: результат парсинга этой строки таблицы
+        """
         insider_name: Optional[str] = selector.xpath('./td[1]/a/text()').extract_first()
         insider_link: Optional[str] = selector.xpath('./td[1]/a/@href').extract_first()
         insider_id: Optional[str] = insider_link.strip('/').split('/')[-1] if insider_link else None
@@ -51,6 +58,7 @@ class RawRow(NamedTuple):
 
 
 class ParsedRow(NamedTuple):
+    """Именнованный кортеж, для хранения строки таблицы в распарсенном виде."""
     insider_name: str
     insider_id: str
     relation: Optional[str]
@@ -64,6 +72,12 @@ class ParsedRow(NamedTuple):
 
     @staticmethod
     def from_raw_row(raw_row: RawRow) -> 'ParsedRow':
+        """Метод, получающий из "сырых" данных обработанные.
+
+        :param raw_row: именнованный кортеж с нужными нам данными в сыром виде
+        :param symbol: название акции ("символ"), к которым относятся эти данные
+        :return: именнованный кортеж с нужными данными в нужном формате, готовыми к отправке "наружу"
+        """
         return ParsedRow(
             insider_name=raw_row.insider_name,
             insider_id=raw_row.insider_id,
@@ -79,18 +93,34 @@ class ParsedRow(NamedTuple):
             symbol=raw_row.symbol,
         )
 
-    def as_dict(self):
+    def as_dict(self) -> Dict:
+        """Прокси для приватного метода `_asdict`.
+
+        :return: именнованный кортеж, преобразованный в словарь
+        """
         return self._asdict()
 
 
 class InsiderTradesSpider(BaseSpider):
+    """"Паук", для получения данных о торгах инсайдеров."""
     name = 'insider_trades'
 
-    def start_requests(self):
+    def start_requests(self) -> Iterator[Request]:
+        """Итератор по первоначальным запросам.
+
+        Делает запрос к странице с историческими ценам для каждой переданной акции.
+        """
         for symbol in self.symbols:
             yield Request(f'https://www.nasdaq.com/symbol/{symbol.lower()}/insider-trades', meta={'symbol': symbol})
 
-    def parse(self, response: Response):
+    def parse(self, response: Response) -> Iterator[Union[Request, Dict]]:
+        """Обработчик http ответа от сайта.
+
+        Парсит таблицы с данными и делает запросы к следующим страницам.
+
+        :param response: ответ, получаемый из Scrapy
+        :return: итератор по словарям с результатами парсинга и по запросам к следующим страницам
+        """
         symbol = response.meta['symbol']
         link_extractor = LinkExtractor(
             allow=rf'https://www\.nasdaq\.com/symbol/{symbol.lower()}/insider-trades\?page=\d+'
