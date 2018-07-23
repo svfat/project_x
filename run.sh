867a339c653b9fa4e7cd05b2808af7df04b97a94
vladimir <vovanzagorodsky@gmail.com>
2018-07-23 17:11:34 +0500

    more readme

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ docker-compose up -d postgres
 docker-compose run --rm wait-for-postgres
 docker-compose run --rm scraper ./migrations/apply.sh
 docker-compose run --rm scraper
-docker-compose up -d
+docker-compose up -d web
